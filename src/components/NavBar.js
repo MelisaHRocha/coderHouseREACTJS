4e3b7b8b59f5201b8e651915075d0caee1839921
DESKTOP-1C1D6R0\Melisa <MHRochaRodriguez@proveedor.teco.com.ar>
2022-09-22 12:06:24 -0300

    Refactor

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,16 +22,18 @@ function NavBar(){
                     <li><a>Reservas</a></li>
                     <li tabIndex={0}>
                         <a>
-                        Parent
+                        Servicios
                         <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
                         </a>
                         <ul className="p-2 bg-primary text-primary-content">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
+                        <li><a>Piscina</a></li>
+                        <li><a>Nature Views</a></li>
+                        <li><a>Galeria</a></li>
                      
                         </ul>
                     </li>
                     <li><a>Contacto</a></li>
+                    <li><a>Ubicacion</a></li>
             
                     </ul>
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">

c585f034e15a9d18934ea16cc2f963fefc46dbf6
DESKTOP-1C1D6R0\Melisa <MHRochaRodriguez@proveedor.teco.com.ar>
2022-09-22 13:17:30 -0300

    Refactor

--- src/components/CardWidget.js
+++ src/components/CardWidget.js
@@ -1,10 +1,17 @@
 import '../App.css';
-import { FaHouseUser } from "react-icons/fa";
 
 function CardWidget(){
     return(
         <>
-             <li><FaHouseUser/></li>
+            <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+                <div className="card-body">
+                    <span className="font-bold text-lg" style={{color: 'black' }}>8 Items</span>
+                    <span className="text-info">Subtotal: $999</span>
+                    <div className="card-actions">
+                        <button className="btn btn-primary btn-block">View cart</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,6 @@
 import '../App.css';
 import CardWidget  from './CardWidget';
 
-const styles = {
-    padding: '10px',
-    display: 'flex',
-    justify: 'center', 
-}
-
-
 function NavBar(){
     return(
         <>
@@ -17,8 +10,7 @@ function NavBar(){
                     <a className="btn btn-ghost normal-case text-xl">Hostel do Moro</a>
                 </div>
                 <div className="flex-none">
-                    <ul className="menu menu-horizontal p-0">
-                    
+                <ul className="menu menu-horizontal p-0">               
                     <li><a>Reservas</a></li>
                     <li tabIndex={0}>
                         <a>
@@ -28,27 +20,40 @@ function NavBar(){
                         <ul className="p-2 bg-primary text-primary-content">
                         <li><a>Piscina</a></li>
                         <li><a>Nature Views</a></li>
-                        <li><a>Galeria</a></li>
-                     
+                        <li><a>Galeria</a></li>                    
                         </ul>
                     </li>
                     <li><a>Contacto</a></li>
-                    <li><a>Ubicacion</a></li>
-            
+                    <li><a>Ubicacion</a></li>         
                     </ul>
+                    <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                        <div className="indicator">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                        <span className="badge badge-sm indicator-item">8</span>
+                        </div>
+                    </label>
+                    <CardWidget></CardWidget>
+                    </div>
+                    <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
                         <img src="https://placeimg.com/80/80/people" />
                         </div>
                     </label>
-                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                        <CardWidget></CardWidget>
-                        </div>
-                    </label>
-                    
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>
+                        <a className="justify-between">
+                            Profile
+                            <span className="badge">New</span>
+                        </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                    </div>
                 </div>
-                </div>
+                </div>               
         </>
     )
 }
